80202d9681718229328f7aa34ce5a7f5be1c493b
CHLNVgame <CHLNVgame@gmail.com>
2017-06-04 16:40:00 +0700

    Add files via upload

---
+++ Assets/Scripts/Enemies/Box.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//from Assets.Scripts.MapLevel.MapScroll;
+public class Box : MonoBehaviour {
+
+    public float Speed;
+    public float timeAction = 0f;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.timeSinceLevelLoad > timeAction)
+            DirectionMove();
+    }
+    protected void DirectionMove()
+    {
+        Vector3 targetMove = transform.position;
+        targetMove.y -= Speed * Time.deltaTime;
+        transform.position = targetMove;
+    }
+}
