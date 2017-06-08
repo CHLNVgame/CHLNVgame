ffa9886cbfd8b10f61be7d6057036e96251e24ed
CHLNVgame <CHLNVgame@gmail.com>
2017-06-08 10:03:05 +0700

    - Modify to call level from prefab

---
+++ Assets/Scripts/MapLevel/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+	public int idLevel;
+	public GameObject[] listLevel = null;
+	// Use this for initialization
+	void Start () {
+		Instantiate (listLevel [idLevel], transform.position, Quaternion.identity);	
+	}
+
+}
---
+++ Assets/Scripts/MapLevel/LoadMap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMap : MonoBehaviour {
+
+	public int idMap;
+	public GameObject[] listMap = null;
+	// Use this for initialization
+	void Start () {
+		Instantiate (listMap [idMap], transform.position, Quaternion.identity);	
+	}
+}
