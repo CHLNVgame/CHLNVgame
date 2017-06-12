318752367de5bb92473d8078618e529ce68887bd
CHLNVgame <CHLNVgame@gmail.com>
2017-06-12 20:33:24 +0700

    + Added GamePlayController

---
+++ Assets/Scripts/MapLevel/GamePlayController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayController : MonoBehaviour {
+    public static GamePlayController instance;
+    [SerializeField]
+    private GameObject PausePanel, VictoryPanel;
+    private bool Victory = false;
+    void Awake()
+    {
+        MakeInstance();
+    }
+    void MakeInstance()
+    {
+        if (instance == null)
+            instance = this;
+    }
+	public void PauseGame()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Continue()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void ShowVictoryPanel()
+    {
+        VictoryPanel.SetActive(true);
+        Victory = true;
+    }
+    public bool GetGameVictory()
+    {
+        return Victory;
+    }
+}
