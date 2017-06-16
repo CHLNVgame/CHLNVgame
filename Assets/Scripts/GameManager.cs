802ae4e3dee41e2dad6f7aed3104f233f2faec0a
CHLNVgame <CHLNVgame@gmail.com>
2017-06-16 17:20:05 +0700

    + Add temp World Map select level

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	public int level;
+
+
+	void Start()
+	{
+		if (instance != null) {
+			return;
+		}
+
+		instance = this;
+		DontDestroyOnLoad (this);
+	}
+
+	public void setLevel (string text)
+	{
+		level = int.Parse (text);
+	}
+}
