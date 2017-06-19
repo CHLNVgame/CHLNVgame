a24877ae0e462b3a5c12b67924c35bade87882be
CHLNVgame <CHLNVgame@gmail.com>
2017-06-19 19:18:05 +0700

    - modify class name

--- Assets/Scripts/Bullets/BulletMc01.cs
+++ Assets/Scripts/Bullets/BulletMc01.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class BulletMc01 : MonoBehaviour {
 	public float Speed;
 	//private float Attack;
 	protected Rigidbody2D bodyBullet;
